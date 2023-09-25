c66b399e5902a9405a0709ac444e997cb86470cc
Joel J George <joeljoby111@gmail.com>
2023-09-25 21:00:25 +0530

    added basic search

--- app/page.js
+++ app/page.js
@@ -1,9 +1,29 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
+import { invoke } from "@tauri-apps/api/tauri";
+import { Input } from "@/components/ui/Input";
 
 export default function Home() {
+  const [searchkeyword, setSearchKeyword] = useState("");
+  const [inputkeyword, setInputkeyword] = useState("search this ");
+  async function greet(name) {
+    setSearchKeyword(await invoke("greet", { name }));
+  }
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div>
+      <Input
+        type="text"
+        placeholder="name"
+        value={inputkeyword}
+        onChange={(e) => {
+          setInputkeyword(e.currentTarget.value);
+          greet(e.currentTarget.value);
+          console.log("hey", searchkeyword);
+        }}
+      />
+      <h1>Home{}</h1>
+      <p>{searchkeyword}</p>
+    </div>
   );
 }
